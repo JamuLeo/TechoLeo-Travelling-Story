d3ede43e3a7985e87417ecef2a2e3abc75b9fa65
JamuLeo <bsc-com-06-21@unima.ac.mw>
2025-06-13 16:42:00 +0100

    upload image api changed

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -7,7 +7,7 @@ import { useNavigate } from 'react-router-dom';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 //import EmptyCard from '../../components/Cards/EmptyCard';
-import EmptyCard from '../../components/Cards/EmptyCard';
+import EmptyCard from '../../../components/Cards/EmptyCard';
 import FilterInfoTitle from '../../components/Cards/FilterInfoTitle';
 import TravelStoryCard from '../../components/Cards/TravelStoryCard';
 import Navbar from '../../components/Navbar';
