7d8a612212987c5f6e64766f2766a253b9531501
JamuLeo <167177522+JamuLeo@users.noreply.github.com>
2025-07-02 03:24:25 +0100

    Update Home.jsx

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -14,9 +14,32 @@ import AddEditTravelStory from './AddEditTravelStory';
 import ViewTravelStory from './ViewTravelStory';
 //import EmptyImg from "../../assets/images/add-story.png";
 
-import EmptyCard from '../../components/Cards/EmptyCard';
+//import EmptyCard from '../../components/Cards/EmptyCard';
 import FilterInfoTitle from '../../components/Cards/FilterInfoTitle';
 import TravelStoryCard from '../../components/Cards/TravelStoryCard';
+
+const EmptyCard = ({ imgSrc, message }) => (
+  <div className="flex flex-col items-center justify-center mt-20">
+    <img
+      src={imgSrc}
+      alt=""
+      className="w-24 flex items-center justify-center bg-cyan-50 rounded-full border border-cyan-100"
+    />
+    <p className="w-1/2 text-sm font-medium text-slate-700 text-center leading-7 mt-5">
+      {message}
+    </p>
+  </div>
+);
+
+
+
+
+
+
+
+
+
+
 
 
 const Home = () => {
